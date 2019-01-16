4884b9ce3cf42fd52d7612dac3ad12b27c192b1b
Patrick Solis <psolis00@gmail.com>
2019-01-16 14:35:02 -0800

    Initial Commit

---
+++ HomeView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeView
+//
+//  Created by Patrick Solis on 1/16/19.
+//  Copyright © 2019 SolistenUp Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
